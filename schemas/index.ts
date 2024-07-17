22bdda86095cbc786144fde376166c14904f9ed0
Mario Niko <marios-2222@hotmail.com>
2024-07-17 21:41:09 +0300

    complete login and register forms and actions

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
         message: "Παμε παλι, 1 2 3 4 5 δεν ειναι δυσκολο"
     })
 });
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Βαλε το email σου και ασε τις μαλακιες"
+    }),
+    password: z.string().min(6,{
+        message: "Βαλε ρε φιλε ενα σωστο pass ολο 12345"
+    }),
+    name: z.string().min(3, {
+        message: "Βαλε ρε φιλε κατι παραπανω"
+    })
+});
