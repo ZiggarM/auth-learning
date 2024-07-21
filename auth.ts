9445d2255f6df792fc84f4b2ed2d595f75219883
Mario Niko <marios-2222@hotmail.com>
2024-07-21 14:43:28 +0300

    add user roles

--- auth.ts
+++ auth.ts
@@ -1,12 +1,43 @@
 import NextAuth from "next-auth"
  import {PrismaAdapter} from "@auth/prisma-adapter"
 
-import {db } from "@/lib/db"
+ import { getUserById } from "@/data/user"
+import { db } from "@/lib/db"
 import authConfig from "@/auth.config"
+import { UserRole } from "@prisma/client"
+
 
 
 export const 
 { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({token, session}) {
+      
+      if (session.user && token.sub) {
+        session.user.role = token.role as UserRole
+      }
+
+      if (session.user && token.sub) {
+        session.user.id = token.sub
+      }
+      return session;
+    },
+    async jwt({ token}) {
+      if(!token.sub) {
+        return token
+      }
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) {
+        return token
+      }
+
+      token.role = existingUser.role
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig
